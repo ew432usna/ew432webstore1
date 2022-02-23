8a4ecafc357004bdfc1d7b7bda9fdffe5c582b70
John Donnal <donnal@usna.edu>
2022-02-23 15:48:22 -0500

    added cart functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
     def index
+        if session[:cart].nil?
+            session[:cart] = []
+        end
         @products = Product.all
+        @cart = session[:cart]
+    end
+
+    def buy
+        if session[:cart].nil?
+            session[:cart] = []
+        end
+        product = Product.find(params[:id])
+        session[:cart].append(product)
+        redirect_to :root
+    end
+
+    def checkout
+        @cart = session[:cart]
+        # empty the shopping cart
+        session[:cart] = []
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 root "home#index"
+get "/products/:id/buy", to: "home#buy", as: "buy"
+get "/checkout", to: "home#checkout", as: "checkout"
 end
